2db94fd3fd99b57d9af60e2f5ae3baf29d033aa8
Rein-Schregardus <rein@schre.nl>
2026-02-17 16:29:53 +0100

    java project creation
    With testcontainer, tymeleaf and lombok

---
+++ hr-cms/src/main/java/nl/hardwerkendenederlanders/hrcms/HrCmsApplication.java
@@ -0,0 +1,13 @@
+package nl.hardwerkendenederlanders.hrcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrCmsApplication.class, args);
+    }
+
+}
---
+++ hr-cms/src/test/java/nl/hardwerkendenederlanders/hrcms/TestHrCmsApplication.java
@@ -0,0 +1,11 @@
+package nl.hardwerkendenederlanders.hrcms;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestHrCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(HrCmsApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
