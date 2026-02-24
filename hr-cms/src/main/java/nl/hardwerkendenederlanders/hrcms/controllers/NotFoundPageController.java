933a36d03ca21fef0224f74163b9ab1aedc3322b
Rein-Schregardus <rein@schre.nl>
2026-02-24 08:41:50 +0100

    filenames casing refactor

--- hr-cms/src/main/java/nl/hardwerkendenederlanders/hrcms/controllers/NotFoundPageController.java
+++ hr-cms/src/main/java/nl/hardwerkendenederlanders/hrcms/controllers/NotFoundPageController.java
@@ -4,12 +4,11 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.webmvc.error.ErrorController;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class notFoundPageController implements ErrorController {
+public class NotFoundPageController implements ErrorController {
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request){
