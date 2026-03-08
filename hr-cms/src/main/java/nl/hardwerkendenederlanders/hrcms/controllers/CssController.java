eae4f1744a03f27db0252064e04f96b518089080
Rein-Schregardus <rein@schre.nl>
2026-03-08 15:51:39 +0100

    css controller
    css controller returns all file paths of css files.

--- hr-cms/src/main/java/nl/hardwerkendenederlanders/hrcms/controllers/CssController.java
+++ hr-cms/src/main/java/nl/hardwerkendenederlanders/hrcms/controllers/CssController.java
@@ -1,42 +1,36 @@
 package nl.hardwerkendenederlanders.hrcms.controllers;
 
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class CssController {
 
-    @GetMapping("/css")
-    public List<Path> getAllCssPaths() {
-        try {
-            ClassLoader cl = this.getClass().getClassLoader();
-            ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(cl);
-            Resource[] resources = resolver.getResources("classpath*:/*.css");
-            String[] uriPaths = Arrays.stream(resources).map(resource -> resource.getURI());
-            for (Resource resource : resources) {
-                resource.getURI();
-            }
+    private final ResourcePatternResolver _resourcePatternResolver;
 
-
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return List.of();
+    public CssController(ResourcePatternResolver resourcePatternResolver) {
+        this._resourcePatternResolver = resourcePatternResolver;
     }
+
+    @GetMapping("/css")
+    public Set<String> getAllCssPaths() throws IOException {
+
+        Resource[] cssResources = _resourcePatternResolver.getResources("classpath:/static/css/**/*.css");
+        var cssSet = new HashSet<String>();
+        for (Resource resource: cssResources){
+            cssSet.add("css/" + resource.getURL().getPath().split("static/css/")[1]);
+        }
+        return cssSet;
+    }
+
 }
