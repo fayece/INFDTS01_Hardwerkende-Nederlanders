aa49dbb1e321ecf2c022ea13d70b95bbe004e3ba
Rein-Schregardus <rein@schre.nl>
2026-03-08 11:52:50 +0100

    css controller wip

--- hr-cms/src/main/java/nl/hardwerkendenederlanders/hrcms/controllers/ReaderHomeController.java
+++ hr-cms/src/main/java/nl/hardwerkendenederlanders/hrcms/controllers/ReaderHomeController.java
@@ -1,14 +1,17 @@
 package nl.hardwerkendenederlanders.hrcms.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.lang.reflect.Array;
 
 @Controller
 public class ReaderHomeController {
 
     @GetMapping("/")
-    public String getHomePage() {
-
-        return "index";
+    public String getHomePage(Model model) {
+        model.addAttribute("articles", new String[] {"foobar", "snuffelaar", "brabbelaar"});
+        return "pages/index";
     }
 }
