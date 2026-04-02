e6d86d5c7e8a3682235b05229f62833c2d4ac956
amari999 <kennashaendir@gmail.com>
2026-04-03 01:23:41 +0200

    AuthService added to authorize the login
    Credentials are validated
    
    Incorrect login shows generic error (to ensure no account details can be brute forced)
    
    Added a passwordhasher in the pom dependencies so we can use a premade hasher

---
+++ hr-cms/src/main/java/nl/hardwerkendenederlanders/hrcms/services/AuthService.java
@@ -0,0 +1,27 @@
+﻿package nl.hardwerkendenederlanders.hrcms.services;
+
+import nl.hardwerkendenederlanders.hrcms.database.interfaces.UserRepository;
+import nl.hardwerkendenederlanders.hrcms.models.User;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class AuthService {
+//    String hash = BCrypt.hashpw(password, BCrypt.gensalt()); dit gebruiken bij account maken om het ww te hashen
+//    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//    String hash = encoder.encode(password);
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User login(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Invalid credentials")); // both the same error so no brute force possible
+        if(!BCrypt.checkpw(password, user.getPasswordHash())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return user;
+    }
+}
