d1a27a5d9fe6ddc8f1d16fe10f475b4463ea624e
Faye <fayecels@gmail.com>
2026-04-15 17:48:57 +0200

    Setting up replies

--- hr-cms/src/main/resources/static/js/fragments/articles/article-page.js
+++ hr-cms/src/main/resources/static/js/fragments/articles/article-page.js
@@ -1,5 +1,6 @@
 import { formatLocalTimes } from './time-formatter.js';
 import { initReplyLoader } from './reply-handler.js';
+import { initReplyForm } from './reply-form-handler.js';
 import { initCommentLoadMore } from './comment-load-more.js';
 import { initCommentForm } from './comment-form-handler.js';
 import { renderMarkdown } from './render-markdown.js';
@@ -7,6 +8,7 @@ import { renderMarkdown } from './render-markdown.js';
 document.addEventListener("DOMContentLoaded", () => {
     formatLocalTimes();
     initReplyLoader();
+    initReplyForm();
     initCommentLoadMore();
     initCommentForm();
     renderMarkdown();
