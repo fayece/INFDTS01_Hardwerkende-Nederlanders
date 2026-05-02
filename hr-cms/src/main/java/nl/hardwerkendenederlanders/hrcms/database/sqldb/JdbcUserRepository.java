1004ec5db4bfd44e99b7fee92decd4a8cabedbde
Rein-Schregardus <rein@schre.nl>
2026-05-02 12:42:49 +0200

    unused import removed

--- hr-cms/src/main/java/nl/hardwerkendenederlanders/hrcms/database/sqldb/JdbcUserRepository.java
+++ hr-cms/src/main/java/nl/hardwerkendenederlanders/hrcms/database/sqldb/JdbcUserRepository.java
@@ -9,7 +9,6 @@ import nl.hardwerkendenederlanders.hrcms.models.User;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Repository;
 
 public class JdbcUserRepository implements UserRepository {
     private final NamedParameterJdbcTemplate jdbc;
