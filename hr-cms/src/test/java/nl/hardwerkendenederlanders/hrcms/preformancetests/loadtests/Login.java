1ab2a879b831d323d5905a674be464d3f4ee1fcf
Rein-Schregardus <rein@schre.nl>
2026-05-22 13:24:35 +0200

    added gatling

---
+++ hr-cms/src/test/java/nl/hardwerkendenederlanders/hrcms/preformancetests/loadtests/Login.java
@@ -0,0 +1,93 @@
+package nl.hardwerkendenederlanders.hrcms.preformancetests.loadtests;
+
+import java.time.Duration;
+import java.util.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+import io.gatling.javaapi.jdbc.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+import static io.gatling.javaapi.jdbc.JdbcDsl.*;
+
+public class Login extends Simulation {
+
+  private String serverPort = System.getProperty("server_port");
+
+  private HttpProtocolBuilder httpProtocol = http
+    .baseUrl("http://localhost:" + serverPort)
+    .disableFollowRedirect()
+    .inferHtmlResources(AllowList(), DenyList(".*\\.js", ".*\\.css", ".*\\.gif", ".*\\.jpeg", ".*\\.jpg", ".*\\.ico", ".*\\.woff", ".*\\.woff2", ".*\\.(t|o)tf", ".*\\.png", ".*\\.svg", ".*detectportal\\.firefox\\.com.*"))
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8")
+    .acceptEncodingHeader("gzip, deflate, br")
+    .acceptLanguageHeader("nl;q=0.7")
+    .upgradeInsecureRequestsHeader("1")
+    .userAgentHeader("Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Mobile Safari/537.36");
+  
+  private Map<CharSequence, String> headers_0 = Map.ofEntries(
+    Map.entry("Cache-Control", "no-cache"),
+    Map.entry("Pragma", "no-cache"),
+    Map.entry("Sec-Fetch-Dest", "document"),
+    Map.entry("Sec-Fetch-Mode", "navigate"),
+    Map.entry("Sec-Fetch-Site", "none"),
+    Map.entry("Sec-Fetch-User", "?1"),
+    Map.entry("Sec-GPC", "1"),
+    Map.entry("sec-ch-ua", "Chromium\";v=\"148\", \"Brave\";v=\"148\", \"Not/A)Brand\";v=\"99"),
+    Map.entry("sec-ch-ua-mobile", "?1"),
+    Map.entry("sec-ch-ua-platform", "Android")
+  );
+  
+  private Map<CharSequence, String> headers_1 = Map.ofEntries(
+    Map.entry("Cache-Control", "no-cache"),
+    Map.entry("Origin", "http://localhost:" + serverPort),
+    Map.entry("Pragma", "no-cache"),
+    Map.entry("Sec-Fetch-Dest", "document"),
+    Map.entry("Sec-Fetch-Mode", "navigate"),
+    Map.entry("Sec-Fetch-Site", "same-origin"),
+    Map.entry("Sec-Fetch-User", "?1"),
+    Map.entry("Sec-GPC", "1"),
+    Map.entry("sec-ch-ua", "Chromium\";v=\"148\", \"Brave\";v=\"148\", \"Not/A)Brand\";v=\"99"),
+    Map.entry("sec-ch-ua-mobile", "?1"),
+    Map.entry("sec-ch-ua-platform", "Android")
+  );
+  
+  private Map<CharSequence, String> headers_2 = Map.ofEntries(
+    Map.entry("Cache-Control", "no-cache"),
+    Map.entry("Pragma", "no-cache"),
+    Map.entry("Sec-Fetch-Dest", "document"),
+    Map.entry("Sec-Fetch-Mode", "navigate"),
+    Map.entry("Sec-Fetch-Site", "same-origin"),
+    Map.entry("Sec-Fetch-User", "?1"),
+    Map.entry("Sec-GPC", "1"),
+    Map.entry("sec-ch-ua", "Chromium\";v=\"148\", \"Brave\";v=\"148\", \"Not/A)Brand\";v=\"99"),
+    Map.entry("sec-ch-ua-mobile", "?1"),
+    Map.entry("sec-ch-ua-platform", "Android")
+  );
+
+
+  private ScenarioBuilder scn = scenario("Login")
+    .exec(
+      http("request_0")
+        .get("/login")
+        .headers(headers_0),
+      pause(2),
+      http("request_1")
+        .post("/login")
+        .headers(headers_1)
+        .formParam("email", "int@int.int")
+        .formParam("password", "int")
+        .check(status().is(302)),
+      pause(1),
+      http("request_3")
+        .get("/")
+        .headers(headers_2)
+        .check(status().is(200))
+    );
+
+  {
+	  setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
+  }
+}
