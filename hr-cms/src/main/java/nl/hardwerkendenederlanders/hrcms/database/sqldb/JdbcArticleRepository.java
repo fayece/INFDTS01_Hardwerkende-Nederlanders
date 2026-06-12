0e2d1344211569732bbd447cc4b7645de513ea3b
Rein-Schregardus <rein@schre.nl>
2026-06-12 18:34:37 +0200

    formatting

--- hr-cms/src/main/java/nl/hardwerkendenederlanders/hrcms/database/sqldb/JdbcArticleRepository.java
+++ hr-cms/src/main/java/nl/hardwerkendenederlanders/hrcms/database/sqldb/JdbcArticleRepository.java
@@ -115,10 +115,41 @@ public class JdbcArticleRepository implements ArticleRepository {
                     "findAllPages was called with a offset of " + offset + " the minimum is 1");
 
         String query = """
-         SELECT * FROM full_articles
-         ORDER BY created_at DESC
-         LIMIT :limit
-         OFFSET :offset;
+             WITH most_recent_authors AS (
+                SELECT *
+                FROM article_authors AS aan
+                WHERE aan.created_at IN
+                (SELECT min(created_at) FROM article_authors GROUP BY article_id)
+            ),
+            desired_article_ids AS (
+                 SELECT id
+                 FROM articles
+                 ORDER BY created_at DESC
+                 LIMIT :limit
+                 OFFSET :offset
+            )
+
+            SELECT
+                a.id as article_id,
+                a.title,
+                a.text_content,
+                a.updated_at,
+                a.created_at,
+                a.publication_status,
+                s.subject_name,
+                count(distinct av.id) as view_count,
+                0 as comment_count,
+                mrs.author_id as first_author_id
+
+            FROM desired_article_ids AS dai
+                LEFT JOIN articles a ON a.id = dai.id
+                LEFT JOIN subjects s ON s.id = a.subject_id
+                LEFT JOIN article_viewers av ON a.id = av.article_id
+                LEFT JOIN most_recent_authors mrs on a.id = mrs.article_id
+
+            GROUP BY a.id, s.id, mrs.author_id, a.created_at
+
+            ORDER BY created_at DESC;
          """;
         MapSqlParameterSource mapping = new MapSqlParameterSource();
         mapping.addValue("limit", limit);
@@ -164,7 +195,7 @@ public class JdbcArticleRepository implements ArticleRepository {
                 count(distinct av.id) as view_count,
                 0 as comment_count,
                 mrs.author_id as first_author_id
- 
+
             FROM desired_article_ids AS dai
                 LEFT JOIN articles a ON a.id = dai.id
                 LEFT JOIN subjects s ON s.id = a.subject_id
@@ -194,12 +225,12 @@ public class JdbcArticleRepository implements ArticleRepository {
     public @Nullable ArticleFullDetailsDto findArticlePublished(UUID id) {
         String query = """
             WITH most_recent_authors AS (
-                SELECT author_id
+                SELECT author_id, article_id
                 FROM article_authors AS aan
                 WHERE aan.article_id = :id AND aan.created_at IN
                 (SELECT min(created_at) FROM article_authors GROUP BY article_id)
             )
-            
+
             SELECT
                 a.id as article_id,
                 a.title,
@@ -211,14 +242,14 @@ public class JdbcArticleRepository implements ArticleRepository {
                 count(distinct av.id) as view_count,
                 0 as comment_count,
                 mrs.author_id as first_author_id
-            
+
             FROM articles AS a
                 LEFT JOIN subjects s ON s.id = a.subject_id
                 LEFT JOIN article_viewers av ON a.id = av.article_id
-                CROSS JOIN most_recent_authors mrs
-            
+                LEFT JOIN most_recent_authors mrs ON mrs.article_id = a.id
+
             WHERE a.publication_status = 'PUBLISHED' AND a.id = :id
-            
+
             GROUP BY a.id, s.id, mrs.author_id;
             """;
         MapSqlParameterSource mapping = new MapSqlParameterSource();
